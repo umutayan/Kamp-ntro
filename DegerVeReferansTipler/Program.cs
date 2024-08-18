1e67217d22bfd68143e60e38ac1546d466854cb7
Umut Ayan <umut_ayan1903@outlook.com>
2024-08-18 17:03:46 +0300

    Kamp

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -10,8 +10,18 @@ namespace DegerVeReferansTipler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
-            Console.WriteLine();
+            //int,bool,float,double değer
+            int say1 = 10;
+            int say2 = 20;
+            say1 = say2;
+            say2 = 30;
+            Console.WriteLine(say1);
+            //interface, class arrays referans
+            int[] sayilar1= new int[] { 10, 20, 30 };
+            int[] sayilar2 = new int[] { 100, 200, 300 };
+            sayilar1 = sayilar2;
+            sayilar2[0] = 999;
+            Console.WriteLine(sayilar1[0]);
         }
     }
 }

ced64264ea3c3803c0cbdf431a17b652a1d5f52b
Umut Ayan <umut_ayan1903@outlook.com>
2024-08-21 00:11:35 +0300

    SQL

---
+++ SQLQuery2.sql
@@ -0,0 +1,11 @@
+﻿--Select
+--ANSII
+Select ContactName Adi, CompanyName Sirketadi, City Sehir from Customers
+
+Select * from Customers where City = 'Berlin'
+
+--case insensitive
+Select * from Products where CategoryID=1 or CategoryID=3
+
+
+

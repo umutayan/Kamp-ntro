820081428d370b42112381b2fd7966fbacd43b1c
Umut Ayan <umut_ayan1903@outlook.com>
2024-08-21 15:23:21 +0300

    Kamp

---
+++ GameProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new  NewEStateUserValidationManager());
+            gamerManager.Add(new Gamer { Id = 1, BirthYear = 2003, FirstName = "Umut", LastName = "AYAN", IdentityNumber = 12345 });
+        
+        }
+    }
+}
---
+++ GameProject/UserValidationManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject
+{
+    internal class UserValidationManager : IUserValidationService
+    {
+        public bool Validate(Gamer gamer)
+        {
+            if (gamer.BirthYear == 2003 && gamer.FirstName == "Umut" && gamer.LastName == "AYAN" && gamer.IdentityNumber == 12345) 
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
